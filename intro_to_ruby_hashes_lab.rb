8e2521b8d7554baedfd3ec10ddb92c6dbb8bb008
Miwha Geschwind <miwhag@gmail.com>
2019-12-29 22:53:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
       railroads: {
         pieces: 4,
         rent_in_dollars: {:key1 => "one",:key2 => "two",:key3 => "three",:key4 => "four"},
-        names: {}
+        names: {:key1 => "one",:key2 => "two",:key3 => "three",:key4 => "four"}
       }
  }
 end

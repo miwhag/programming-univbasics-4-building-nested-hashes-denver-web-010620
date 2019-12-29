b186fbeb2cf01c199f3b056e79a7cb533ca85d47
Miwha Geschwind <miwhag@gmail.com>
2019-12-29 23:04:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
  monopoly = {
       railroads: {
         pieces: 4,
-        rent_in_dollars: {:one_piece_owned => 25,:two_pieces_owned => 50,:three_pieces_owned => 100,:key4 => "four"},
+        rent_in_dollars: {:one_piece_owned => 25,:two_pieces_owned => 50,:three_pieces_owned => 100,:four_pieces_owned => 200},
         names: {:key1 => "one",:key2 => "two",:key3 => "three",:key4 => "four"},
         one_piece_owned: {:fee => 25 } 
       }

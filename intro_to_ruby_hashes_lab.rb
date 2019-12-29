eb7dd75f9cec843075337100245ccd70ce84a2ff
Miwha Geschwind <miwhag@gmail.com>
2019-12-29 22:40:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
     }
     
     monopoly_with_second_tier[:monopoly][:railroads] = :pieces 
-    
+    monopoly_with_second_tier[:monopoly][:railroads] = monopoly_with_second_tier.delete(:railroads)
 end
 
 def monopoly_with_third_tier
